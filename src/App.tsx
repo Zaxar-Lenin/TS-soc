41fe854fc5af68b710ffec4ee6183436eafd6d61
Zaxar <zaxar137samohval44@gmail.com>
2022-01-06 16:50:07 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Reaact from "./react"
+import Titlle from "./Titlle"
+import Acordion from './Acordion';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "all">
+        {/* <Reaact value = {3} />
+        <Titlle title = {'sisi'}/>
+        <Reaact value = {5} /> */}
+        <Acordion title = 'fdgfdfdf' collabsed = {true} />
+        {/* <Titlle title = {'efeffefe'}/>
+        <Titlle title = {'grgrgrgrg'}/>
+        <Titlle title = {'w0ooooooooooooooooo'}/> */}
+        <Acordion title = {'We fdgfdfdf'} collabsed = {false} />
     </div>
   );
 }
